ca4602e1e581e2a1fb98225d84ecc080c1438d11
dangeromoralvi <omoralvi01568@gmail.com>
2024-10-06 23:20:40 +0600

    Update 22 info.js

--- scripts/commands/22 info.js
+++ scripts/commands/22 info.js
@@ -24,13 +24,13 @@ const time = process.uptime(),
     seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【hh:mm:ss】");
-var link = ["https://i.imgur.com/Yi0QXnn.jpeg", 
+var link = ["https://i.imgur.com/srZRXmK.jpeg", 
 
-            "https://i.imgur.com/Yi0QXnn.jpeg", 
+            "https://i.imgur.com/srZRXmK.jpeg", 
 
-            "https://i.imgur.com/Yi0QXnn.jpeg",
+            "https://i.imgur.com/srZRXmK.jpeg",
 
-            "https://i.imgur.com/Yi0QXnn.jpeg"];
+            "https://i.imgur.com/srZRXmK.jpeg"];
 
 var callback = () => api.sendMessage({body:`•—»✨𝐀𝐝𝐦𝐢𝐧 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✨🌺
  •┄┅════❁🌺❁════┅┄•
