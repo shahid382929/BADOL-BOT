836d882e6157bdccb53c3f521a36e9b897868202
dangeromoralvi <omoralvi01568@gmail.com>
2024-10-07 02:51:45 +0600

    Update fuck4.js

--- scripts/commands/fuck4.js
+++ scripts/commands/fuck4.js
@@ -1,18 +1,18 @@
 module.exports.config = {
-    name: "fuckv4",
-    version: "3.1.1",
-    hasPermssion: 0,
-    credits: "BADOL-KHAN",
-    description: "Get fuck",
-    commandCategory: "Admin",
-    usages: "[@mention]",
-    cooldowns: 5,
-    dependencies: {
-        "axios": "",
-        "fs-extra": "",
-        "path": "",
-        "jimp": ""
-    }
+  name: "fuck4",
+  version: "1.0.1", 
+  permssion: 2,
+  credits: "★𝐌𝟗𝐇𝟒𝐌𝐌𝟒𝐃-𝐁𝟒𝐃𝟗𝐋★",
+  prefix:true,
+  description: "Admin and Bot info.",
+  category: "...",
+  cooldowns: 1,
+  dependencies: 
+  {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
 };
 
 module.exports.onLoad = async() => {
